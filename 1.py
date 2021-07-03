db51fa7b0b4b472d9fd1808bb3f780bc30d89ce7
rithikraj <rithikraj365@gmail.com>
2021-07-03 13:30:04 +0530

    2nd commit for push

--- 1.py
+++ 1.py
@@ -1,2 +1,3 @@
 print("Hello this 1")
 print("This is changed to 2.1")
+print("Now an chaged by 2 person to 2.1")
--- 2.py
+++ 2.py
@@ -1,2 +1,3 @@
 print("Hello this is 2")
 print("this is changed to 2.2")
+print("Now an chaged by 2 person to 2.2")
